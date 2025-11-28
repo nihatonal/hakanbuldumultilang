72ec460a9cfa3c37e4019abf77117c6e272748c7
nihatonal <onalnihat1986@gmail.com>
2025-11-28 15:52:20 +0300

    updated error

--- app/blog-sitemap.xml/route.ts
+++ app/blog-sitemap.xml/route.ts
@@ -1,22 +1,33 @@
 import { NextResponse } from "next/server";
 import { client } from "@/sanity/lib/client";
 
+interface BlogItem {
+  slug: { current: string };
+  publishedAt?: string;
+}
+
 export async function GET() {
   const baseUrl = "https://www.hakanbuldu.com";
 
-  const blogs = await client.fetch(`*[_type=="blog"]{slug, publishedAt}`);
+  const blogs = await client.fetch<BlogItem[]>(
+    `*[_type=="blog"]{slug, publishedAt}`
+  );
 
-  let urls = "";
-
-  blogs.forEach((blog: any) => {
-    urls += `
+  const urls = blogs
+    .map(
+      (blog) => `
 <url>
   <loc>${baseUrl}/tr/blog/${blog.slug.current}</loc>
-  <lastmod>${blog.publishedAt || new Date().toISOString()}</lastmod>
+  <lastmod>${
+    blog.publishedAt
+      ? new Date(blog.publishedAt).toISOString()
+      : new Date().toISOString()
+  }</lastmod>
   <changefreq>monthly</changefreq>
   <priority>0.7</priority>
-</url>`;
-  });
+</url>`
+    )
+    .join("");
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
